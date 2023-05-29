5e8c315ebd0d183755d42f70c630f3356648b023
Baraded <borodeda@gmail.com>
2023-05-29 20:17:51 +0300

    add midle

---
+++ observer/Midle.java
@@ -0,0 +1,27 @@
+public class Midle implements Observer{
+    private String vacancy;
+    private String name;
+    private double salary;
+
+    public Midle(String name){
+        this.name = name;
+        salary = 90000;
+        this.vacancy = vacancy;
+        vacancy = "developer";
+    }
+
+
+
+    @Override
+    public void receiveOffer(String companyName, double salary) {
+        if (this.salary < salary){
+            System.out.printf("Мастер %s >>> Мне нужна эта работа! [%s - %f]\n", name, companyName, salary);
+            this.salary = salary;
+        }
+        else {
+            System.out.printf("Мастер %s >>> Я найду работу получше! [%s - %f]\n", name, companyName, salary);
+        }
+        
+    }
+
+}
