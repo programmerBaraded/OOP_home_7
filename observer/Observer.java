a84c27d6f3c325191c3d6822315dd669e83e394a
Baraded <borodeda@gmail.com>
2023-05-29 19:34:04 +0300

    start

---
+++ observer/Observer.java
@@ -0,0 +1,9 @@
+
+/**
+ * Интерфейс наблюдателя (соискателя, человек в поисках работы)
+ */
+public interface Observer {
+
+    void receiveOffer(String companyName, double salary);
+
+}
---
+++ observer/Publisher.java
@@ -0,0 +1,10 @@
+
+public interface Publisher {
+
+    void registerObserver(Observer observer);
+
+    void removeObserver(Observer observer);
+
+    void sendOffer(String nameCompany, double salary);
+
+}
