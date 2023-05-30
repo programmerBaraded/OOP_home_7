9b9767a2e0c9b64eac31845110fc0a0ac2650bbf
Baraded <borodeda@gmail.com>
2023-05-30 12:33:44 +0300

    over time

---
+++ observer/Vacancy.java
@@ -0,0 +1,17 @@
+public class Vacancy {
+    private String vacancy;
+    private int salary;
+
+    public Vacancy(String vacancy, int salary) {
+        this.vacancy = vacancy;
+        this.salary = salary;
+    }
+
+    public String getVacancy() {
+        return vacancy;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+}
